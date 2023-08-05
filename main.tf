e76038f830e9b89ffebad354b9a6099d08779d51
thaiminhpv <thaiminhpv@gmail.com>
2023-08-05 18:18:35 +0700

    update metadata project sshkeys

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 resource "google_compute_project_metadata" "default" {
+  provider = google.longnv
+  metadata = {
+    ssh-keys = file("./ssh-keys.txt")
+  }
+}
+
+resource "google_compute_project_metadata" "default" {
+  provider = google.minhpvt
   metadata = {
     ssh-keys = file("./ssh-keys.txt")
   }
