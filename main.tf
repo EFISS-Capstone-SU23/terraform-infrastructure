9cde1bc6a1f34d82e27597ac458953c3541706b9
thaiminhpv <thaiminhpv@gmail.com>
2023-08-05 18:22:23 +0700

    fix google_compute_project_metadata must be unique

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "google_compute_project_metadata" "default" {
+resource "google_compute_project_metadata" "default_longnv" {
   provider = google.longnv
   metadata = {
     ssh-keys = file("./ssh-keys.txt")
   }
 }
 
-resource "google_compute_project_metadata" "default" {
+resource "google_compute_project_metadata" "default_minhpvt" {
   provider = google.minhpvt
   metadata = {
     ssh-keys = file("./ssh-keys.txt")
