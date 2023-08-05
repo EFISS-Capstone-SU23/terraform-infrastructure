f4edebc4b8609c84fc4397b69ba4a594241fe296
thaiminhpv <thaiminhpv@gmail.com>
2023-08-05 13:36:11 +0700

    update variables

--- variables.tf
+++ variables.tf
@@ -7,3 +7,15 @@ variable "zone" {
   type    = string
   default = "asia-southeast1-b"
 }
+
+variable "github_runner_token" {
+  type    = string
+}
+
+variable "worker_join_token" {
+    type    = string
+}
+
+variable "worker_join_hash_discover" {
+    type    = string
+}
