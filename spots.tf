7818442c1e601fef8e85e76e1f94089324eb5806
thaiminhpv <thaiminhpv@gmail.com>
2023-08-09 00:12:50 +0700

    add anhnd spot worker with new found e2-highmem-8  (8 vCPUs, 64 GB memory)

--- spots.tf
+++ spots.tf
@@ -3,7 +3,7 @@ resource "google_compute_instance" "spot_worker_longnv" {
   count    = 0
   provider = google.longnv
 
-  machine_type = "e2-small"
+  machine_type = "e2-highmem-8"  # 8 vCPU, 64GB RAM
 
   boot_disk {
     auto_delete = true
@@ -67,7 +67,7 @@ resource "google_compute_instance" "spot_worker_minhpvt" {
   count    = 0
   provider = google.minhpvt
 
-  machine_type = "e2-small"
+  machine_type = "e2-highmem-8"  # 8 vCPU, 64GB RAM
 
   boot_disk {
     auto_delete = true
@@ -113,6 +113,70 @@ resource "google_compute_instance" "spot_worker_minhpvt" {
 
   service_account {
     email  = "910878745565-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = var.zone
+}
+
+resource "google_compute_instance" "worker_anhnd" {
+  name     = "worker-anhnd-${count.index}"
+  count    = 0
+  provider = google.anhnd
+
+  machine_type = "e2-highmem-8"  # 8 vCPU, 64GB RAM
+
+  boot_disk {
+    auto_delete = true
+    device_name = "efiss"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-11-bullseye-v20230711"
+      size  = 80
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+    type        = "worker"
+  }
+
+  metadata = {
+    ssh-keys       = file("./ssh-keys.txt")
+    startup-script = "curl -s https://raw.githubusercontent.com/EFISS-Capstone-SU23/k8s-manifest/main/scripts/gcp-scripts/gce-worker-setup.sh | bash -s ${var.worker_join_token} ${var.worker_join_hash_discover}"
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/reflected-coder-395210/regions/${var.region}/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = false
+    on_host_maintenance = "TERMINATE"
+    preemptible         = true
+    provisioning_model  = "SPOT"
+  }
+
+  service_account {
+    email  = "235045321665-compute@developer.gserviceaccount.com"
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
