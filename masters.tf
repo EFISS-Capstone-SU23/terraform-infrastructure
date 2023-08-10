25804ab75ffd2870ebfaa82b5f9b59e17ac82f8e
thaiminhpv <thaiminhpv@gmail.com>
2023-08-11 00:00:45 +0700

    Revert "spin test"
    This reverts commit 0b194488ddea5ba5a9b68623fd62c373915ae983.

--- masters.tf
+++ masters.tf
@@ -65,7 +65,7 @@ resource "google_compute_instance" "master_longnv" {
 
 resource "google_compute_instance" "master_minhpvt" {
   name     = "master-minhpvt-${count.index}"
-  count    = 1
+  count    = 0
   provider = google.minhpvt
 
   machine_type = "e2-small"
