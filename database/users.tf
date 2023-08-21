e3ce237eb2dbe2ebdcdc1e2d29343018bf1d2392
thaiminhpv <thaiminhpv@gmail.com>
2023-08-21 09:33:34 +0700

    change to root user with host=% instead

--- database/users.tf
+++ database/users.tf
@@ -1,31 +1,31 @@
 resource "google_sql_user" "admin_user" {
   provider = google.minhpvt
-  name     = "admin_user"
+  name     = "root"
   instance = google_sql_database_instance.admin_instance.name
-  host     = "admin.db.efiss.tech"
+  host     = "%"
   password = var.db_sql_password
 }
 
 resource "google_sql_user" "auth_user" {
   provider = google.minhpvt
-  name     = "auth_user"
+  name     = "root"
   instance = google_sql_database_instance.auth_instance.name
-  host     = "auth.db.efiss.tech"
+  host     = "%"
   password = var.db_sql_password
 }
 
 resource "google_sql_user" "ads_user" {
   provider = google.minhpvt
-  name     = "ads_user"
+  name     = "root"
   instance = google_sql_database_instance.ads_instance.name
-  host     = "ads.db.efiss.tech"
+  host     = "%"
   password = var.db_sql_password
 }
 
 resource "google_sql_user" "normal_user" {
   provider = google.minhpvt
-  name     = "normal_user"
+  name     = "root"
   instance = google_sql_database_instance.normal_user_instance.name
-  host     = "normal-db.efiss.tech"
+  host     = "%"
   password = var.db_sql_password
 }
