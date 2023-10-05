395572e4a240a847ddbf53b5951bc9e8792e4785
thaiminhpv <thaiminhpv@gmail.com>
2023-10-06 00:26:41 +0700

    spin 1 instance 150 disk 6 vCPU 24GB RAM

--- masters.tf
+++ masters.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "master_minhpvt" {
   name     = "master-minhpvt-${count.index}"
-  count    = 0
+  count    = 1
   provider = google.minhpvt
 
   #   machine_type = "e2-small"
