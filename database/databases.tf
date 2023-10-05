3d4663e352976aaec5fa57dbfecf55534425d031
thaiminhpv <thaiminhpv@gmail.com>
2023-10-06 00:23:14 +0700

    merge db blueprint

--- database/databases.tf
+++ database/databases.tf
@@ -1,105 +1,14 @@
-# efiss-admin
-resource "google_sql_database" "admin" {
+resource "google_sql_database" "mysql" {
   provider = google.minhpvt
-  name     = "efiss-admin"
-  instance = google_sql_database_instance.admin_instance[0].name
-  count    = length(google_sql_database_instance.admin_instance)
+  name     = "efiss-mysql"
+  instance = google_sql_database_instance.mysql_instance[0].name
+  count    = length(google_sql_database_instance.mysql_instance)
 }
 
 # See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
-resource "google_sql_database_instance" "admin_instance" {
+resource "google_sql_database_instance" "mysql_instance" {
   provider         = google.minhpvt
-  name             = "efiss-admin"
-  region           = var.region
-  database_version = "MYSQL_8_0"
-  count            = 0
-  settings {
-    tier = "db-f1-micro"
-    location_preference {
-      zone = var.zone
-    }
-    ip_configuration {
-      authorized_networks {
-        name  = "allow-all"
-        value = "0.0.0.0/0"
-      }
-    }
-  }
-
-  deletion_protection = "false"
-}
-
-
-# efiss-auth
-resource "google_sql_database" "auth" {
-  provider = google.minhpvt
-  name     = "efiss-auth"
-  instance = google_sql_database_instance.auth_instance[0].name
-  count    = length(google_sql_database_instance.auth_instance)
-}
-resource "google_sql_database_instance" "auth_instance" {
-  provider         = google.minhpvt
-  name             = "efiss-auth"
-  region           = var.region
-  database_version = "MYSQL_8_0"
-  count            = 0
-  settings {
-    tier = "db-f1-micro"
-    location_preference {
-      zone = var.zone
-    }
-    ip_configuration {
-      authorized_networks {
-        name  = "allow-all"
-        value = "0.0.0.0/0"
-      }
-    }
-  }
-
-  deletion_protection = "false"
-}
-
-# efiss-normal-user
-resource "google_sql_database" "normal_user" {
-  provider = google.minhpvt
-  name     = "efiss-normal-user"
-  instance = google_sql_database_instance.normal_user_instance[0].name
-  count    = length(google_sql_database_instance.normal_user_instance)
-}
-
-resource "google_sql_database_instance" "normal_user_instance" {
-  provider         = google.minhpvt
-  name             = "efiss-normal-user"
-  region           = var.region
-  database_version = "MYSQL_8_0"
-  count            = 0
-  settings {
-    tier = "db-f1-micro"
-    location_preference {
-      zone = var.zone
-    }
-    ip_configuration {
-      authorized_networks {
-        name  = "allow-all"
-        value = "0.0.0.0/0"
-      }
-    }
-  }
-
-  deletion_protection = "false"
-}
-
-# efiss-ads
-resource "google_sql_database" "ads" {
-  provider = google.minhpvt
-  name     = "efiss-ads"
-  instance = google_sql_database_instance.ads_instance[0].name
-  count    = length(google_sql_database_instance.ads_instance)
-}
-
-resource "google_sql_database_instance" "ads_instance" {
-  provider         = google.minhpvt
-  name             = "efiss-ads"
+  name             = "efiss-mysql"
   region           = var.region
   database_version = "MYSQL_8_0"
   count            = 0
--- database/users.tf
+++ database/users.tf
@@ -1,35 +1,8 @@
-resource "google_sql_user" "admin_user" {
+resource "google_sql_user" "mysql_user" {
   provider = google.minhpvt
   name     = "root"
-  instance = google_sql_database_instance.admin_instance[0].name
-  count    = length(google_sql_database_instance.admin_instance)
-  host     = "%"
-  password = var.db_sql_password
-}
-
-resource "google_sql_user" "auth_user" {
-  provider = google.minhpvt
-  name     = "root"
-  instance = google_sql_database_instance.auth_instance[0].name
-  count    = length(google_sql_database_instance.auth_instance)
-  host     = "%"
-  password = var.db_sql_password
-}
-
-resource "google_sql_user" "ads_user" {
-  provider = google.minhpvt
-  name     = "root"
-  instance = google_sql_database_instance.ads_instance[0].name
-  count    = length(google_sql_database_instance.ads_instance)
-  host     = "%"
-  password = var.db_sql_password
-}
-
-resource "google_sql_user" "normal_user" {
-  provider = google.minhpvt
-  name     = "root"
-  instance = google_sql_database_instance.normal_user_instance[0].name
-  count    = length(google_sql_database_instance.normal_user_instance)
+  instance = google_sql_database_instance.mysql_instance[0].name
+  count    = length(google_sql_database_instance.mysql_instance)
   host     = "%"
   password = var.db_sql_password
 }
