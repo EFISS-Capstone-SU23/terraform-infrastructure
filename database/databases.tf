064673fb39997ae5572952d9f9a864249e60e98e
thaiminhpv <thaiminhpv@gmail.com>
2023-10-06 00:29:11 +0700

    output db ip address

--- database/databases.tf
+++ database/databases.tf
@@ -27,3 +27,7 @@ resource "google_sql_database_instance" "mysql_instance" {
 
   deletion_protection = "false"
 }
+
+output "ip_address" {
+  value = google_sql_database_instance.mysql_instance[0].ip_address
+}
